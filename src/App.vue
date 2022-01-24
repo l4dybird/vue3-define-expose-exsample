325333b4f35348690b4bac403380c04f42fdbb90
yukimaruDubstep <k.yuuki1997@gmail.com>
2022-01-24 15:32:15 +0900

    :tada: Vue3 Composition API script setup で子コンポーネント内に実装している関数を親コンポーネント内で呼び出す実装

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Child from './components/Child.vue';
+
+// 子コンポーネント側のコンポーネント情報を受け取る ref を宣言しています。
+const childRef = ref();
+
+// 子コンポーネント側に渡す props を宣言しています。
+const childValue = "Hello, Zenn";
+
+// 子コンポーネント側のメソッドを発火させるメソッドを実装しています。
+const onChildMethodClick = () => {
+    childRef.value.childMethod();
+};
+</script>
+
+<template>
+    <div>
+        <!-- Child コンポーネントに ref の childRef を付与します -->
+        <Child ref="childRef" :value="childValue" />
+        <button @click="onChildMethodClick">子コンポーネント内のメソッドを呼び出す</button>
+    </div>
+</template>
---
+++ src/components/Child.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// 子コンポーネントで受け取る Props の型を宣言しています。
+type Props = {
+    value: string;
+};
+
+// 子コンポーネントで受け取る Props を宣言しています。
+const { value } = defineProps<Props>();
+
+// defineExpose に宣言したい関数を実装しています。
+const childMethod = (): void => {
+    console.log(`子コンポーネントの value に ${value} が Props で渡されいます。`);
+};
+
+// defineExpose を使用してコンポーネント内に定義されたメソッドを親コンポーネントから参照できる様にしています。
+defineExpose({
+  childMethod,
+});
+</script>
+
+<template>
+    <div>
+        {{ value }}
+    </div>
+</template>
